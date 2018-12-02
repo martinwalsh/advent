c1fc76f2c5a1b45c1a58ecaaaa861fba14e82f7a
Martin Walsh <martin.walsh@ahold.com>
2018-12-02 06:09:36 -0600

    add go solution for day 01

---
+++ golang/src/day01.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"log"
+	"strconv"
+)
+
+// https://softwareengineering.stackexchange.com/questions/177428/sets-data-structure-in-golang
+type IntSet struct {
+	set map[int]bool
+}
+
+func NewIntSet() *IntSet {
+	return &IntSet{make(map[int]bool)}
+}
+
+func (set *IntSet) Add(i int) bool {
+	_, found := set.set[i]
+	set.set[i] = true
+	return !found
+}
+
+func (set *IntSet) Contains(i int) bool {
+	_, found := set.set[i]
+	return found
+}
+
+//
+
+func calibrate(seq []string, freq int) chan int {
+	c := make(chan int)
+
+	go func() {
+		for _, value := range seq {
+			v, err := strconv.Atoi(value)
+			if err != nil {
+				log.Fatal(err)
+			}
+			freq += v
+			c <- freq
+		}
+		close(c)
+	}()
+
+	return c
+}
+
+func day01_part1(seq []string) int {
+	// FIXME: hrm, there must be a better way to grab the last element
+	var result int
+	for freq := range calibrate(seq, 0) {
+		result = freq
+	}
+	return result
+}
+
+func day01_part2(seq []string) int {
+	seen := NewIntSet()
+	seen.Add(0)
+
+	retries := 1
+	current := 0
+
+	for retries < 1000 {
+		for freq := range calibrate(seq, current) {
+			if seen.Contains(freq) {
+				return freq
+			}
+			seen.Add(freq)
+			current = freq
+		}
+		retries += 1
+	}
+
+	log.Fatal("Invalid input: reached retry limit")
+
+	return 0
+}
---
+++ golang/src/day01_test.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"github.com/franela/goblin"
+	. "github.com/onsi/gomega"
+	"log"
+	"os"
+	"testing"
+)
+
+func load_data(path string) []string {
+	var lines []string
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return lines
+}
+
+func TestDay01Part1(t *testing.T) {
+	g := goblin.Goblin(t)
+
+	RegisterFailHandler(func(m string, _ ...int) { g.Fail(m) })
+
+	g.Describe("check part 1 examples", func() {
+		g.It("should correctly adjust frequencies", func() {
+			example1 := []string{"+1", "+1", "+1"}
+			example2 := []string{"+1", "+1", "-2"}
+			example3 := []string{"-1", "-2", "-3"}
+
+			Expect(day01_part1(example1)).To(Equal(3))
+			Expect(day01_part1(example2)).To(Equal(0))
+			Expect(day01_part1(example3)).To(Equal(-6))
+		})
+	})
+
+	g.Describe("real input for part 1", func() {
+		g.It("should correctly adjust frequencies", func() {
+			Expect(day01_part1(load_data("input/day01.txt"))).To(Equal(582))
+		})
+	})
+}
+
+func TestDay01Part2(t *testing.T) {
+	g := goblin.Goblin(t)
+
+	RegisterFailHandler(func(m string, _ ...int) { g.Fail(m) })
+
+	g.Describe("check part 2 examples", func() {
+		g.It("should correctly calibrate", func() {
+			Expect(day01_part2([]string{"+1", "-1"})).To(Equal(0))
+			Expect(day01_part2([]string{"+3", "+3", "+4", "-2", "-4"})).To(Equal(10))
+			Expect(day01_part2([]string{"-6", "+3", "+8", "+5", "-6"})).To(Equal(5))
+			Expect(day01_part2([]string{"+7", "+7", "-2", "-7", "-4"})).To(Equal(14))
+		})
+	})
+
+	g.Describe("real input for part 2", func() {
+		g.It("should correctly calibrate", func() {
+			Expect(day01_part2(load_data("input/day01.txt"))).To(Equal(488))
+		})
+	})
+}
