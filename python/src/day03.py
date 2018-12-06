ab82745f2c39a36ac33440ef22d896c24a4bfa07
Martin Walsh <marty@notoptimal.com>
2018-12-05 22:31:39 -0600

    update python solution for day 03

--- python/src/day03.py
+++ python/src/day03.py
@@ -1,61 +1,56 @@
 #!/usr/bin/env python
 import re
 from itertools import product
-from collections import namedtuple
+from collections import defaultdict
+
+# updated with improvements from
+# https://github.com/petertseng/adventofcode-rb-2018/blob/master/03_grid_claims.rb
 
 
 class Fabric(object):
-    pattern = re.compile(r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)')
-    claim = namedtuple('Claim', ['id', 'left', 'top', 'width', 'height'])
+    def __init__(self):
+        self.squares = defaultdict(list)
+        self.all_ids = set()
 
-    def __init__(self, maxx, maxy):
-        self.squares = {}
-        self.maxx = maxx
-        self.maxy = maxy
-        for x, y in product(range(maxx), range(maxy)):
-            self.squares[(x, y)] = -1
+    def place(self, claim):
+        _id, left, top, width, height = claim
+        for x, y in product(range(left, left + width),
+                            range(top, top + height)):
+            self.squares[(x, y)].append(_id)
+            self.all_ids.add(_id)
 
-    def place(self, claim, attempt=0):
-        it_overlaps = False
-        for x, y in product(range(claim.left, claim.left + claim.width),
-                            range(claim.top, claim.top + claim.height)):
-            self.squares[(x, y)] = self.squares[(x, y)] + 1
-            if self.squares[(x, y)] > attempt:
-                it_overlaps = True
-        return it_overlaps
-
-    def get_overlap(self):
-        for x, y in product(range(self.maxx), range(self.maxy)):
-            if self.squares[(x, y)] > 0:
-                yield (x, y)
+    @property
+    def overlap(self):
+        return [ids for (x, y), ids in self.squares.items() if len(ids) > 1]
 
     @classmethod
     def from_records(cls, records):
         claims = []
+        pattern = re.compile(r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)')
+
         for record in records:
             try:
                 _id, left, top, width, height = \
-                        map(int, cls.pattern.search(record).groups())
+                        map(int, pattern.search(record).groups())
             except (AttributeError, ValueError):
                 raise ValueError(f'Invalid claim record format: {record}')
-            claims.append(cls.claim(_id, left, top, width, height))
+            claims.append((_id, left, top, width, height))
 
-        maxx = max(c.left + c.width for c in claims)
-        maxy = max(c.top + c.height for c in claims)
-        return cls(maxx, maxy), claims
+        fabric = cls()
+        for claim in claims:
+            fabric.place(claim)
+
+        return fabric
 
 
 def part1(records):
-    fabric, claims = Fabric.from_records(records)
-    for claim in claims:
-        fabric.place(claim)
-    return len(list(fabric.get_overlap()))
+    fabric = Fabric.from_records(records)
+    return len(fabric.overlap)
 
 
 def part2(records):
-    fabric, claims = Fabric.from_records(records)
-    no_overlaps = [claim for claim in claims if not fabric.place(claim)]
-    while no_overlaps:
-        claim = no_overlaps.pop()
-        if not fabric.place(claim, 1):
-            return claim.id
+    fabric = Fabric.from_records(records)
+    overlap_ids = set()
+    for ids in fabric.overlap:
+        overlap_ids.update(ids)
+    return fabric.all_ids.difference(overlap_ids).pop()
--- python/tests/test_day03.py
+++ python/tests/test_day03.py
@@ -1,21 +1,18 @@
-import pytest
-from day03 import part1, part2, Fabric
+from day03 import part1, part2
 
-EXAMPLE1 = [
-    ("#1 @ 1,3: 4x4", (1, 1, 3, 4, 4)),
-    ("#2 @ 3,1: 4x4", (2, 3, 1, 4, 4)),
-    ("#3 @ 5,5: 2x2", (3, 5, 5, 2, 2)),
+EXAMPLE = [
+    "#1 @ 1,3: 4x4",
+    "#2 @ 3,1: 4x4",
+    "#3 @ 5,5: 2x2",
 ]
 
 
-@pytest.mark.parametrize('record,expected', EXAMPLE1)
-def test_examples_record_parser(record, expected):
-    fabric, claims = Fabric.from_records([record])
-    assert claims[0] == expected
-
-
 def test_examples_overlap():
-    assert part1([X[0] for X in EXAMPLE1]) == 4
+    assert part1(EXAMPLE) == 4
+
+
+def test_examples_no_overlap():
+    assert part2(EXAMPLE) == 3
 
 
 def test_solution_part1(load_input):
