751b9a885294289f5b1fd23194aa32dcb6a16208
Martin Walsh <marty@notoptimal.com>
2019-12-13 06:02:19 -0600

    add solution for day 01 part2

--- elixir/lib/day01.ex
+++ elixir/lib/day01.ex
@@ -1,20 +1,34 @@
 defmodule Day01 do
   def part1() do
-    data()
-    |> Enum.reduce(0, fn n, acc -> acc + calculate_fuel(n) end)
+    Enum.map(data(), &calculate_fuel/1) |> sum()
+  end
+
+  def part2() do
+    Enum.map(data(), &calculate_fuel_with_fuel_cost/1) |> sum()
   end
 
   def calculate_fuel(mass) do
     Kernel.trunc(mass / 3) - 2
   end
 
+  def calculate_fuel_with_fuel_cost(mass) do
+    case calculate_fuel(mass) do
+      n when n <= 0 ->
+        0
+
+      n ->
+        n + calculate_fuel_with_fuel_cost(n)
+    end
+  end
+
+  defp sum(numbers) do
+    Enum.reduce(numbers, 0, fn n, acc -> n + acc end)
+  end
+
   defp data do
     File.read!("/data/day01-2019.txt")
-    |> String.split(~r/\R/)
-    |> Enum.filter(&(&1 != ""))
-    |> Enum.map(fn line ->
-      {n, ""} = Integer.parse(line)
-      n
-    end)
+    |> String.split("\n")
+    |> Enum.drop(-1)
+    |> Enum.map(&String.to_integer/1)
   end
 end
--- elixir/test/day01_test.exs
+++ elixir/test/day01_test.exs
@@ -2,11 +2,24 @@ defmodule Day01Test do
   use ExUnit.Case
   import Day01
 
-  test "example_part1" do
+  test "examples_part1" do
     assert calculate_fuel(12) == 2
+    assert calculate_fuel(14) == 2
+    assert calculate_fuel(1969) == 654
+    assert calculate_fuel(100_756) == 33583
+  end
+
+  test "example_part2" do
+    assert calculate_fuel_with_fuel_cost(14) == 2
+    assert calculate_fuel_with_fuel_cost(1969) == 966
+    assert calculate_fuel_with_fuel_cost(100_756) == 50346
   end
 
   test "part1" do
     assert part1() == 3_317_659
+  end
+
+  test "part2" do
+    assert part2() == 4_973_616
   end
 end
