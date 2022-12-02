828258670deb521d7ae1829028428953b143ec14
Martin Walsh <1520570+martinwalsh@users.noreply.github.com>
2022-12-01 22:34:03 -0600

    update clojure scaffolding

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,5 +1,5 @@
 (defproject adventofcode "0.1.0-SNAPSHOT"
   :description "Advent of Code Scaffolding"
   :url "https://github.com/martinwalsh/advent"
-  :dependencies [[org.clojure/clojure "1.9.0"]])
-
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :plugins [[lein-eftest "0.6.0"]])
