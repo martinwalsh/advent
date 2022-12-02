afe10919f706dbd5303d9fd8cac3751e0d0c4bc6
Martin Walsh <1520570+martinwalsh@users.noreply.github.com>
2022-12-01 23:36:13 -0600

    touch up test template (#9)

--- clojure/test/dayNN_test.clj
+++ clojure/test/dayNN_test.clj
@@ -1,12 +1,24 @@
 (ns dayNN-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [deftest is testing use-fixtures]]
             dayNN))
+
+;; https://stuartsierra.com/2016/05/19/fixtures-as-caches
+(def ^:dynamic data)
+(defn load-data [f]
+  (binding [data (slurp "/data/dayNN.txt")] 
+    (f)))
+(use-fixtures :each load-data)
+
+(def EXAMPLE1 (clojure.string/trim "
+"))
+
+(deftest dayNN-test-examples
+  (is (and
+    (= nil (dayNN/part1 nil))
+    (= nil (dayNN/part2 nil)))))
 
 (deftest dayNN-part1-test
   (is (= nil (dayNN/part1 nil))))
 
 (deftest dayNN-part2-test
   (is (= nil (dayNN/part2 nil))))
-
-(deftest dayNN-can-read-a-data-file
-  (is (= "" (slurp "/data/dayNN.txt"))))
