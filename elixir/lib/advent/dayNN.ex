36a12a26e07991751750a094d4b261d193d68896
Martin Walsh <1520570+martinwalsh@users.noreply.github.com>
2022-12-02 19:28:27 -0600

    update elixir scaffolding

--- elixir/lib/advent/dayNN.ex
+++ elixir/lib/advent/dayNN.ex
@@ -1,2 +1,11 @@
 defmodule Advent.DayNN do
+  @spec part1(String.t) :: integer
+  def part1(_data) do
+    nil
+  end
+
+  @spec part2(String.t) :: integer
+  def part2(_data) do
+    nil
+  end
 end
--- elixir/mix.exs
+++ elixir/mix.exs
@@ -5,7 +5,7 @@ defmodule Advent.MixProject do
     [
       app: :advent,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       deps_path: "/tmp/deps",
