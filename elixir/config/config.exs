02a99013087458efe2ac786f70ab33b2578e5fcd
Martin Walsh <1520570+martinwalsh@users.noreply.github.com>
2022-12-02 19:29:12 -0600

    update elixir scaffolding (#12)

--- elixir/config/config.exs
+++ elixir/config/config.exs
@@ -1,4 +1,4 @@
-use Mix.Config
+import Config
 
 config :advent,
   year: DateTime.utc_now().year - if(DateTime.utc_now().month == 12, do: 1, else: 0)
--- elixir/test/dayNN_test.exs
+++ elixir/test/dayNN_test.exs
@@ -3,28 +3,35 @@ defmodule DayNNTest do
   import Advent.DayNN
 
   @moduletag :dayNN
+  @example1 """
+  """
+
+  setup_all do
+    {:ok, data} = File.read("/data/dayNN.txt")
+    {:ok, data: data}
+  end
 
   describe "examples" do
     @tag examples: :part1
     test "part1" do
-      assert true
+      assert part1(@example1) == nil
     end
 
     @tag examples: :part2
     test "part2" do
-      assert true
+      assert part2(@example1) == nil
     end
   end
 
   describe "solutions" do
     @tag solutions: :part1
-    test "part1" do
-      assert true
+    test "part1", state do
+      assert part1(state[:data]) == nil
     end
 
     @tag solutions: :part2
-    test "part2" do
-      assert true
+    test "part2", state do
+      assert part2(state[:data]) == nil
     end
   end
 end
