c067f3c8429b8e057b07b791e17206c82a6347f1
Martin Walsh <1520570+martinwalsh@users.noreply.github.com>
2022-12-04 13:01:34 -0600

    improve python scaffolding (#14)

--- python/tests/test_dayNN.py
+++ python/tests/test_dayNN.py
@@ -1,15 +1,19 @@
 from dayNN import part1, part2
 
+EXAMPLE1 = """\
+"""
+
 
 def test_examples():
-    assert True, 'Add tests for the example data described in the puzzle'
+    assert part1(EXAMPLE1) is NotImplemented
+    assert part2(EXAMPLE1) is NotImplemented
 
 
 def test_solution_part1(load_input):
-    with load_input('/data/dayNN.txt') as data:
+    with load_input("/data/dayNN.txt") as data:
         assert part1(data) is NotImplemented
 
 
 def test_solution_part2(load_input):
-    with load_input('/data/dayNN.txt') as data:
+    with load_input("/data/dayNN.txt") as data:
         assert part2(data) is NotImplemented
